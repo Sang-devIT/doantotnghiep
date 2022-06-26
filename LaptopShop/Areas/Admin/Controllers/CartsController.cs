d5c0d80ddcb862f153e06055e56750bbecaf92db
84348 <thanhlevt7@gmail.com>
2022-06-26 22:29:03 +0700

    update chuc nang va giao dien admin

---
+++ LaptopShop/Areas/Admin/Controllers/CartsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LaptopShop.Areas.Admin.Data;
+using LaptopShop.Areas.Admin.Models;
+
+namespace LaptopShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CartsController : Controller
+    {
+        private readonly LaptopshopContext _context;
+
+        public CartsController(LaptopshopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Carts
+        public async Task<IActionResult> Index()
+        {
+            var laptopshopContext = _context.Carts.Include(c => c.Products).Include(c => c.User);
+            return View(await laptopshopContext.ToListAsync());
+        }
+
+        // GET: Admin/Carts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Carts
+                .Include(c => c.Products)
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return View(cart);
+        }
+
+        // GET: Admin/Carts/Create
+        public IActionResult Create()
+        {
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Id");
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Admin/Carts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserID,ProductId,Quantity,Price")] Cart cart)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cart);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Id", cart.ProductId);
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", cart.UserID);
+            return View(cart);
+        }
+
+        // GET: Admin/Carts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Carts.FindAsync(id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Id", cart.ProductId);
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", cart.UserID);
+            return View(cart);
+        }
+
+        // POST: Admin/Carts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserID,ProductId,Quantity,Price")] Cart cart)
+        {
+            if (id != cart.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cart);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CartExists(cart.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Id", cart.ProductId);
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", cart.UserID);
+            return View(cart);
+        }
+
+        // GET: Admin/Carts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Carts
+                .Include(c => c.Products)
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return View(cart);
+        }
+
+        // POST: Admin/Carts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cart = await _context.Carts.FindAsync(id);
+            _context.Carts.Remove(cart);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CartExists(int id)
+        {
+            return _context.Carts.Any(e => e.Id == id);
+        }
+    }
+}
